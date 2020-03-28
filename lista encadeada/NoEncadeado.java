5b565b0e09fccd170a74875d19daaa0f62d1fd04
claudio-code <suporte2424@outlook.com>
2020-03-27 23:25:25 -0300

    lista encadeada

---
+++ lista encadeada/NoEncadeado.java
@@ -0,0 +1,24 @@
+package com.encadeada;
+
+public class NoEncadeado {
+	private int elemento;
+	private NoEncadeado proximo;
+	
+	public int getElemento() {
+		return elemento;
+	}
+
+	public void setElemento(int elemento) {
+		this.elemento = elemento;
+	}
+	
+	
+	public NoEncadeado getProximo() {
+		return proximo;
+	}
+
+	public void setProximo(NoEncadeado proximo) {
+		this.proximo = proximo;
+	}	
+
+}
